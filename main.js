b2972892d39160010299c3e3eacb7ea4d31df8bf
TaroAkz <makararoeum@gmail.com>
2025-08-14 23:50:44 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var title = "កញ្ញា កែវ ក័ន្យារ៉ាត់";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const guestName = title; // Example name
+  document.getElementById("guest-title").textContent = guestName;
+});
